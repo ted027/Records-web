6224e20d9cecc59ae13f67f28c149932fa79ee1e
ted027 <imaitt0207@gmail.com>
2018-08-28 22:50:44 +0900

    [update]url

--- functions/lambda_function.py
+++ functions/lambda_function.py
@@ -4,5 +4,19 @@ lambda handler
 import os
 import sys
 
+import requests
+import bs4
+import json
+
 def lambda_handler(event, context):
-    pass
+    baseurl = 'http://baseballdata.jp/'
+    curl = baseurl + 'ctop'
+    cpurl = baseurl + 'cptop'
+    purl = baseurl + 'ptop'
+    ppurl = baseurl + 'pptop'
+
+
+    res = requests.get(url)
+    res.raise_for_status()
+
+    soup = bs4.BeautifulSoup(res.text, "html.parser")
