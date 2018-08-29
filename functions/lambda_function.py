19f2d112562d0774c20bb8554b75e73cc0be4c18
ted027 <imaitt0207@gmail.com>
2018-08-29 16:25:37 +0900

    wip:handler

--- functions/lambda_function.py
+++ functions/lambda_function.py
@@ -15,8 +15,7 @@ def lambda_handler(event, context):
     purl = baseurl + 'ptop'
     ppurl = baseurl + 'pptop'
 
-
-    res = requests.get(url)
+    # for c cp p pp
+    res = requests.get(curl)
     res.raise_for_status()
-
-    soup = bs4.BeautifulSoup(res.text, "html.parser")
+    soup = bs4.BeautifulSoup(res.content, "html.parser")
