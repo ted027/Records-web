76a2c7cae0d25fbd8c15277829c5bd4839e58ec2
ted027 <imaitt0207@gmail.com>
2018-12-16 20:26:26 +0900

    w

--- commands/records_dev/aws/dynamodb.py
+++ commands/records_dev/aws/dynamodb.py
@@ -1,4 +1,11 @@
 import boto3
 
-def write(HASH_KEY, RANGE_KEY):
-    pass
+class dynamodb:
+    def __init__(self):
+        dynamo = boto3.client('dynamodb')
+
+    def put(HASH_KEY, RANGE_KEY):
+        response = client.put_item(
+            TableName=table_name,
+            Item=item
+        }
