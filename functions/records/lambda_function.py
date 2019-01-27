63b42be5f8cb4334d03c3a6d91bb86b3db65e847
ted027 <imaitt0207@gmail.com>
2019-01-27 10:09:44 +0900

    logger

--- functions/records/lambda_function.py
+++ functions/records/lambda_function.py
@@ -8,6 +8,13 @@ import requests
 import bs4
 import json
 
+from logging import getLogger, StreamHandler, DEBUG
+logger = getLogger(__name__)
+handler = StreamHandler()
+handler.setLevel(DEBUG)
+logger.setLevel(DEBUG)
+logger.addHandler(handler)
+logger.propagate = False
 
 def lambda_handler(event, context):
     pass
