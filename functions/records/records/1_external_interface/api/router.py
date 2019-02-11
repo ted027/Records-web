d1eedf94c5f346accb9e83d5a190ee455dbf5bd1
ted027 <imaitt0207@gmail.com>
2019-02-12 08:26:16 +0900

    init

---
+++ functions/records/records/1_external_interface/api/router.py
@@ -0,0 +1,9 @@
+from backup.controller.api import  ...and
+
+class Router():
+
+    @route(function_id='h01', path='/api/...', methon='get')
+    def brabra(self, ):
+        """
+        """
+        pass
